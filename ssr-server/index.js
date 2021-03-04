5a0d7ae9ca987fbfb047e67e9320989d1ccc582f
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-03-04 16:41:39 -0600

    Autentication con Twitter

--- ssr-server/index.js
+++ ssr-server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const passport = require("passport");
+const session = require("express-session");
 const boom = require("@hapi/boom");
 const cookieParser = require("cookie-parser");
 const axios = require("axios");
@@ -15,12 +16,18 @@ const TWO_HOURS_IN_SEC = 7200000;
 // body parser
 app.use(express.json());
 app.use(cookieParser());
+app.use(session({ secret: config.sessionSecret }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 //  Basic strategy
 require("./utils/auth/strategies/basic");
 
 // OAuth strategy
 require("./utils/auth/strategies/oauth");
+
+// Twitter strategy
+require("./utils/auth/strategies/twitter");
 
 app.post("/auth/sign-in", async function(req, res, next) {
   // Obtenemos el atributo rememberMe desde el cuerpo del request
@@ -152,6 +159,27 @@ app.get(
 app.get(
   "/auth/google/callback",
   passport.authenticate("google", { session: false }),
+  function(req, res, next) {
+    if (!req.user) {
+      next(boom.unauthorized());
+    }
+
+    const { token, ...user } = req.user;
+
+    res.cookie("token", token, {
+      httpOnly: !config.dev,
+      secure: !config.dev
+    });
+
+    res.status(200).json(user);
+  }
+);
+
+app.get("/auth/twitter", passport.authenticate("twitter"));
+
+app.get(
+  "/auth/twitter/callback",
+  passport.authenticate("twitter", { session: false }),
   function(req, res, next) {
     if (!req.user) {
       next(boom.unauthorized());
